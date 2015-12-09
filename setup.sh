21a98d05674a8e868a2da43192925342db0ffd19
Matt Congel <congel@chapman.edu>
2015-12-09 15:27:08 -0800

    gettin started

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /usr/local/.git/hooks
+
+printf 'toplevel_path=`git rev-parse --show-toplevel`\nafplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &' >> /usr/local/.git/hooks/post-commit
