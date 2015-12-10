ce1cf9d8190dbc7adf2a469e264a38ff753becc6
Matt Congel <congel@chapman.edu>
2015-12-09 16:28:52 -0800

    first try/last try

--- setup.sh
+++ setup.sh
@@ -3,8 +3,6 @@ ROOT_DIR=`git rev-parse --show-toplevel`
 cd $ROOT_DIR/.git/hooks
 curl -O https://raw.githubusercontent.com/homeofmatt/commit-cheers/master/happykids.wav
 
-#printf '#!/bin/sh\ntoplevel_path=`git rev-parse --show-toplevel`\nafplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &' >> $ROOT_DIR/.git/hooks/post-commit
-
 cat << EOF >> $ROOT_DIR/.git/hooks/post-commit
 #!/bin/sh
 toplevel_path=\`git rev-parse --show-toplevel\`
