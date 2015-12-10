d9743265b7f22cead209e2fb9822a90ccfdc81be
Matt Congel <congel@chapman.edu>
2015-12-09 16:15:34 -0800

    ditched heredoc for now

--- setup.sh
+++ setup.sh
@@ -3,13 +3,7 @@ ROOT_DIR=`git rev-parse --show-toplevel`
 cd $ROOT_DIR/.git/hooks
 curl -O https://raw.githubusercontent.com/homeofmatt/commit-cheers/master/happykids.wav
 
-#printf '#!/bin/sh\ntoplevel_path=`git rev-parse --show-toplevel`\nafplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &' >> $ROOT_DIR/.git/hooks/post-commit
-
-printf << EOF >> $ROOT_DIR/.git/hooks/post-commit
-#!/bin/sh
-toplevel_path=`git rev-parse --show-toplevel`
-afplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &
-EOF
+printf '#!/bin/sh\ntoplevel_path=`git rev-parse --show-toplevel`\nafplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &' >> $ROOT_DIR/.git/hooks/post-commit
 
 chmod 755 $ROOT_DIR/.git/hooks/post-commit
 cd -
