0151e2346cf6b5463e36431db98a5a375d999005
Matt Congel <congel@chapman.edu>
2015-12-09 16:12:31 -0800

    dropped from global to local directory

--- setup.sh
+++ setup.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
-cd /usr/local/.git/hooks
+ROOT_DIR=`git rev-parse --show-toplevel`
+cd $ROOT_DIR/.git/hooks
 curl -O https://raw.githubusercontent.com/homeofmatt/commit-cheers/master/happykids.wav
-printf 'toplevel_path=`git rev-parse --show-toplevel`\nafplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &' >> /usr/local/.git/hooks/post-commit
-chmod 755 /usr/local/.git/hooks/post-commit
+
+#printf '#!/bin/sh\ntoplevel_path=`git rev-parse --show-toplevel`\nafplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &' >> $ROOT_DIR/.git/hooks/post-commit
+
+printf << EOF >> $ROOT_DIR/.git/hooks/post-commit
+#!/bin/sh
+toplevel_path=`git rev-parse --show-toplevel`
+afplay -v 0.1 $toplevel_path/.git/hooks/happykids.wav > /dev/null 2>&1 &
+EOF
+
+sudo chmod 755 $ROOT_DIR/.git/hooks/post-commit
 cd -
